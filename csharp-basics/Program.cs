5aa46d462e868e4d066378c4206a5a6c57ec8946
Niayesh99 <n.tabrikk@gmail.com>
2025-01-28 11:41:49 +0100

    String Interpolation

--- csharp-basics/Program.cs
+++ csharp-basics/Program.cs
@@ -105,4 +105,7 @@ string middle = "the kid climbed a tree";
 string end = "Everyone lived happily ever after";
 string story = beginning + ", " + middle + ". " + end + ".";
 Console.WriteLine(story);
+string story1 = $"{beginning} it was a beautiful day. {middle} and the birds flew through the sky. {end}";
+
+Console.WriteLine(story1);
 
--- csharp-basics/obj/Debug/net8.0/csharp-basics.AssemblyInfo.cs
+++ csharp-basics/obj/Debug/net8.0/csharp-basics.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp-basics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+420294de673257cf1bda6f4f46aa70beb8796594")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72ac0924739ee40ebdc35b709dfb59ad72799662")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp-basics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharp-basics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
